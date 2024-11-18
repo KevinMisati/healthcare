25746d8a4705271159c54f3dd7509bc56b658ee5
KevinMisati <kevinmosigisi2001@gmail.com>
2024-11-18 18:21:55 +0300

    added the patients form section

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { ID, InputFile, Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 
 import {
   BUCKET_ID,
@@ -59,38 +59,39 @@ export const registerPatient = async ({
   identificationDocument,
   ...patient
 }: RegisterUserParams) => {
-  // try {
-  //   // Upload file ->  // https://appwrite.io/docs/references/cloud/client-web/storage#createFile
-  //   let file;
-  //   if (identificationDocument) {
-  //     const inputFile =
-  //       identificationDocument &&
-  //       InputFile.fromBlob(
-  //         identificationDocument?.get("blobFile") as Blob,
-  //         identificationDocument?.get("fileName") as string
-  //       );
+  try {
+    // Upload file ->  // https://appwrite.io/docs/references/cloud/client-web/storage#createFile
+    let file;
+    if (identificationDocument) {
+      const inputFile =
+        identificationDocument &&
+        InputFile.fromBlob(
+          identificationDocument?.get("blobFile") as Blob,
+          identificationDocument?.get("fileName") as string
+        );
 
-  //     file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
-  //   }
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+    }
 
-  //   // Create new patient document -> https://appwrite.io/docs/references/cloud/server-nodejs/databases#createDocument
-  //   const newPatient = await databases.createDocument(
-  //     DATABASE_ID!,
-  //     PATIENT_COLLECTION_ID!,
-  //     ID.unique(),
-  //     {
-  //       identificationDocumentId: file?.$id ? file.$id : null,
-  //       identificationDocumentUrl: file?.$id
-  //         ? `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file.$id}/view??project=${PROJECT_ID}`
-  //         : null,
-  //       ...patient,
-  //     }
-  //   );
+    // Create new patient document -> https://appwrite.io/docs/references/cloud/server-nodejs/databases#createDocument
+    console.log(patient,"hello patient here")
+    const newPatient = await databases.createDocument(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        identificationDocumentId: file?.$id ? file.$id : null,
+        identificationDocumentUrl: file?.$id
+          ? `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file.$id}/view??project=${PROJECT_ID}`
+          : null,
+        ...patient,
+      }
+    );
 
-  //   return parseStringify(newPatient);
-  // } catch (error) {
-  //   console.error("An error occurred while creating a new patient:", error);
-  // }
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.error("An error occurred while creating a new patient:", error);
+  }
 };
 
 // GET PATIENT
